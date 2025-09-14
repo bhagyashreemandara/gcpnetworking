dc86f40ccbc75a9c87d0ae431bd6717b1627eb2e
bhagyashreemandara <mandaraa18@gmail.com>
2025-09-14 06:52:43 +0000

    add changes

---
+++ var.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+  default     = "strange-passage-470705-j1"
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+  default     = "us-central1"
+}
